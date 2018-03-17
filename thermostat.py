9aec0e22ea14c615b64c14f1103d705a82411a58
Cody Black <codyblack@Codys-MBP-4.attlocal.net>
2018-03-16 19:32:49 -0500

    IP changes

--- thermostat.py
+++ thermostat.py
@@ -1,18 +1,17 @@
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
-from subprocess import call
+import subprocess
 from time import sleep
 
 sensor = 'P9_40'
 temperature_up = 'P9_11'
 temperature_down = 'P9_13'
-IP_Address = call('ip addr show usb0 | grep inet | grep usb0')
-print(IP_Address)
+IP_Address = subprocess.check_output('ip addr show | grep inet')
+print("System IP Address Information: \n {}".format(IP_Address))
 
 ADC.setup()
 GPIO.setup(temperature_up,GPIO.IN)
 GPIO.setup(temperature_down,GPIO.IN)
-
 while True:
         reading = ADC.read(sensor)
         millivolts = reading * 1800  # 1.8V reference = 1800 mV
