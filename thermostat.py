ed56ad5aa356d264fcb9866edc941bf0b72539fc
Cody Black <codyblack@Codys-MBP-4.attlocal.net>
2018-03-16 21:49:16 -0500

    making progress

--- thermostat.py
+++ thermostat.py
@@ -3,23 +3,52 @@ import Adafruit_BBIO.GPIO as GPIO
 import subprocess
 from time import sleep
 
+debug = 1 # change to 1 for debug statements.
+
+# pins
 sensor = 'P9_40'
-temperature_up = 'P9_11'
-temperature_down = 'P9_13'
+temperatureUp = 'P9_11'
+temperatureDown = 'P9_13'
+
+# values that reflect the GPIO input values
+upButtonValue = 0
+downButtonValue = 0
+
+# use subprocess to store IP information in IP_Address
 ps = subprocess.Popen(['ip','addr','show'], stdout=subprocess.PIPE)
 IP_Address = subprocess.check_output(('grep', 'inet'), stdin=ps.stdout)
 ps.wait()
-print("System IP Address Information: \n {}".format(IP_Address))
+if debug:
+    print("System IP Address Information: \n {}".format(IP_Address))
 
+# Use subprocess to store uptime info in UpTimeStr. Parse upTimeStr for exact time.
+upTimeStr = subprocess.check_output(['uptime'])
+index = str.find("up")
+if debug:
+    print("System Uptime: \n {}".format(upTimeStr[index+2:index+6]))
+
+# Use subprocess to store current date/time.
+date = subprocess.check_output(['date'])
+if debug:
+    print("System Date: \n {}".format(date))
 ADC.setup()
-GPIO.setup(temperature_up,GPIO.IN)
-GPIO.setup(temperature_down,GPIO.IN)
-while True:
-        reading = ADC.read(sensor)
-        millivolts = reading * 1800  # 1.8V reference = 1800 mV
-        celsius = (millivolts - 500) / 10
-        far = (celsius * 9/5) + 32
-        print('mv=%d C=%d F=%d' % (millivolts, celsius, far))
-        print(GPIO.input(temperature_up))
-        print(GPIO.input(temperature_down))
-        sleep(1)
+GPIO.setup(temperatureUp,GPIO.IN)
+GPIO.setup(temperatureDown,GPIO.IN)
+
+#update()
+
+# update temperature values and button values
+def update():
+    while True:
+            reading = ADC.read(sensor)
+            millivolts = reading * 1800  # 1.8V reference = 1800 mV
+            celsius = (millivolts - 500) / 10
+            far = (celsius * 9/5) + 32
+            print('mv=%d C=%d F=%d' % (millivolts, celsius, far))
+            upValue = GPIO.input(temperatureUp)
+            downValue = GPIO.input(temperatureDown)
+            if debug:
+                print(GPIO.input(temperatureUp))
+                print(GPIO.input(temperatureDown))
+
+            sleep(1)
