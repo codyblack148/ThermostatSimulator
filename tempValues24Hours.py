e4bef2229a28503f4be5a2a82cc17fbf5a9970b3
Cody Black <cblack@beaglebone.localdomain>
2018-03-17 16:08:02 -0500

    Working, tested with smaller numbers.

--- tempValues24Hours.py
+++ tempValues24Hours.py
@@ -21,20 +21,20 @@ minTemp = celsius
 hourTemperatureList.append(celsius)
 dayTemperatureList.append(celsius)
 # add another while loop with another timer for every layer added, example: years, decades
-while dayTimer < 20:  #86400 seconds is 24 hours
+while dayTimer < 86400:  #86400 seconds is 24 hours
     hourTimer = 0
     listTracker = 0
     avgTempHour = sum(hourTemperatureList)/len(hourTemperatureList)
     dayTemperatureList.append(avgTempHour)
     avgTemp24Hours = sum(dayTemperatureList)/len(dayTemperatureList)
-    while hourTimer < 10: # 3600 seconds is 1 hour
+    while hourTimer < 3600: # 3600 seconds is 1 hour
         reading = ADC.read(sensor)
         celsius = ((reading*1800) - 500) / 10
         hourTemperatureList.append(celsius)
         #print(celsius)
         #print(hourTemperatureList[listTracker])
-        print(minTemp)
-        print(maxTemp)
+        #print(minTemp)
+        #print(maxTemp)
         if hourTemperatureList[listTracker] > maxTemp:
             maxTemp = hourTemperatureList[listTracker]
             #print(maxTemp)
@@ -43,6 +43,6 @@ while dayTimer < 20:  #86400 seconds is 24 hours
             #print(minTemp)
         print('HourAvg=%f DayAvg=%f Min=%f Max=%f' % (avgTempHour, avgTemp24Hours,minTemp,maxTemp ))
         listTracker += 1
-        dayTimer += 5
-        hourTimer += 5
-        sleep(2) #take readings every two minutes
+        dayTimer += 120
+        hourTimer += 120
+        sleep(120) #take readings every two minutes
