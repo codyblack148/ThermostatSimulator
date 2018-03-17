8fecd2d10e4a3455516c3393f83323574dc9ff21
Cody Black <codyblack@Codys-MBP-4.attlocal.net>
2018-03-16 19:04:25 -0500

    testing for system IP address

--- thermostat.py
+++ thermostat.py
@@ -1,17 +1,20 @@
 import Adafruit_BBIO.ADC as ADC
 import Adafruit_BBIO.GPIO as GPIO
+from subprocess import call
 from time import sleep
 
-sensor_pin = 'P9_40'
+sensor = 'P9_40'
 temperature_up = 'P9_11'
 temperature_down = 'P9_13'
+IP_Address = call('ip addr show usb0 | grep inet | grep usb0')
+print(IP_Address)
 
 ADC.setup()
 GPIO.setup(temperature_up,GPIO.IN)
 GPIO.setup(temperature_down,GPIO.IN)
 
 while True:
-        reading = ADC.read(sensor_pin)
+        reading = ADC.read(sensor)
         millivolts = reading * 1800  # 1.8V reference = 1800 mV
         celsius = (millivolts - 500) / 10
         far = (celsius * 9/5) + 32
