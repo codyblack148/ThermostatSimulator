ee5b4776a7a843efe676d42890165471faf2980c
Cody Black <codyblack@Codys-MBP-4.attlocal.net>
2018-03-17 15:44:38 -0500

    debug

--- tempValues24Hours.py
+++ tempValues24Hours.py
@@ -4,18 +4,19 @@ import Adafruit_BBIO.ADC as ADC
 #initialize variables
 dayTimer = 0
 hourTimer = 0
-maxTemp = 0
-# start at 100 degrees C for minTemp... if this causes an issue in the future, please
-# reconsider where your beaglebone is located. It's probably on fire.
-minTemp = 100
 avgTempHour = 0
 avgTemp24Hours = 0
 hourTemperatureList = [0]
 dayTemperatureList = [0]
 listTracker = 0
 
-sensor = 'P9_40'
 ADC.setup()
+#initialize temps to first reading
+reading = ADC.read(sensor)
+celsius = ((reading*1800) - 500) / 10
+sensor = 'P9_40'
+maxTemp = celsius
+minTemp = celsius
 # add another while loop with another timer for every layer added, example: years, decades
 while dayTimer < 86400:  #86400 seconds is 24 hours
     hourTimer = 0
